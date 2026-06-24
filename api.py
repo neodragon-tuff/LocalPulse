60388a2fa9f831b96f06ddb98efd737728dacda0
Akshith Voleti <voletiakshith@gmail.com>
2026-06-24 07:49:26 -0400

    Update api.py

--- api.py
+++ api.py
@@ -44,7 +44,8 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://neodragon-tuff.github.io", "http://localhost:3000", "*"],
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
